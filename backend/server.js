7f9fe3560fb4be61745d75f515c3ed224d96b716
jw <jannik.wienberg@tn.techstarter.de>
2025-03-14 17:03:45 +0100

    a
    a

--- backend/server.js
+++ backend/server.js
@@ -9,51 +9,37 @@ const app = express();
 //TODO: Verbinde eine Datenbank dazu
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+});
 
-app.use(cors());                // Middleware
-app.use(bodyParser.json());     // Middleware (wie ein Übersetzer)
+app.use(cors()); // Middleware
+app.use(bodyParser.json()); // Middleware (wie ein Übersetzer)
 
 //TODO: Schreibe requests/responses
-
 
 // Liste mir alle existierende Items
 // hier sollte nur alle Items als JSON im Response geschrieben werden
 app.get('/liste_abrufen', async (req, res) => {
-    const result = await pool.query('SELECT * FROM tasks')
-    res.json(result.rows)
+	const result = await pool.query('SELECT * FROM tasks');
+	res.json(result.rows);
 });
 
 // Wenn ein neues Item hinzugefügt werden soll, soll NodeJS Server diesen Request so behandeln:
-// app.post('/add', async (req, res) => {
-//     console.log("POST kommt an")
-//     const result = await pool.query('INSERT INTO tasks (title) VALUES ($1)', [req.body.title])
-//     console.log(result.rows) // !hier ist noch etwas falsch
-//     res.json(result.rows)
-
-
-//     // db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
-//     //     res.json({id: this.lastID, title: req.body.title, completed: 0});
-//     // });
-
-// });
-
 app.post('/add', async (req, res) => {
-    const result = await pool.query(
-      'INSERT INTO tasks (title) VALUES ($1) RETURNING *',
-      [req.body.title]
-    );
-    res.json(result.rows[0]);
-  
-    // db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
-    //     res.json({id: this.lastID, title: req.body.title, completed: 0});
-    // });
-  });
+	const result = await pool.query(
+		'INSERT INTO tasks (title) VALUES ($1) RETURNING *',
+		[req.body.title]
+	);
+	res.json(result.rows[0]);
+
+	// db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+	//     res.json({id: this.lastID, title: req.body.title, completed: 0});
+	// });
+});
 
 app.delete('/delete/:id', async (req, res) => {
 	try {
@@ -62,7 +48,7 @@ app.delete('/delete/:id', async (req, res) => {
 		]);
 
 		if (result.rowCount !== 1) {
-			return res.status(404).json({ error: 'Task not found' });
+			return res.status(404).json({ error: 'Error Row Count' });
 		}
 		res.json({ id: req.params.id });
 	} catch (err) {
@@ -73,11 +59,6 @@ app.delete('/delete/:id', async (req, res) => {
 	}
 });
 
-// app.delete('/delete/:id', (req, res) => {
-//     db.run('DELETE FROM tasks WHERE id = ?', req.params.id, () =>{res.json({message: "Eingabe gelöscht"})});
-// })
-
-
-app.listen(3050, "localhost", () => {
-    console.log("bald wird es Mittagspause")
+app.listen(3050, 'localhost', () => {
+	console.log('bald wird es Mittagspause');
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,36 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [title, setTitle] = useState("")
+	const [tasks, setTasks] = useState([]);
+	const [title, setTitle] = useState('');
 
-  useEffect(() => {
-    fetch("http://localhost:3050/liste_abrufen")
-    .then((res) => res.json())
-    .then(setTasks)
-  }, []);
+	useEffect(() => {
+		fetch('http://localhost:3050/liste_abrufen')
+			.then((res) => res.json())
+			.then(setTasks);
+	}, []);
 
-  const itemHinzufuegen = () => {
+	const itemHinzufuegen = () => {
+		//Option 1 für Eingabecheck: falls Eingabefeld leer ist, mach nicht weiter
 
-    //Option 1 für Eingabecheck: falls Eingabefeld leer ist, mach nicht weiter
+		if (!title) {
+			return;
+		}
 
-    if (!title) {
-      return;
-    }
+		fetch('http://localhost:3050/add', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ title }),
+		})
+			// hier möchte ich, dass die Liste in der App auch aktualisiert wird
+			.then((res) => res.json())
+			.then((neueAufgabe) => setTasks([...tasks, neueAufgabe]));
 
-    fetch("http://localhost:3050/add", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({title}),
-    })      
-    // hier möchte ich, dass die Liste in der App auch aktualisiert wird
-      .then((res) => res.json())
-      .then((neueAufgabe) => setTasks([...tasks, neueAufgabe]))
+		setTitle('');
+	};
 
-    setTitle("");
-  }
-  
-  // const itemLoeschen = (id_nummer) => {
-  //   //console.log("Gedrückte Taste:" + id_nummer);
-  //   fetch(`http://localhost:3050/delete/${id_nummer}`, {
-  //     method: "DELETE",
-  //   })
-  // }
-
-  const itemLoeschen = (id_nummer) => {
+	const itemLoeschen = (id_nummer) => {
 		//console.log("Gedrückte Taste:" + id_nummer);
 		fetch(`http://localhost:3050/delete/${id_nummer}`, {
 			method: 'DELETE',
@@ -50,26 +42,27 @@ function App() {
 			.then((filteredTasks) => setTasks(filteredTasks));
 	};
 
-
-  return (
-    <>
-      <h1>To-Do List</h1>
-      <input value={title}  onChange={(e)=>setTitle(e.target.value)} />
-      <button disabled={!title.trim()} onClick={itemHinzufuegen}>Add</button> {/* Option 2 für Eingabecheck: Button wird disabled bleiben wenn das Eingabefeld leer ist*/}
-
-      <ul>
-        {// hier gehört der Code, um die To-Do Liste dynamisch zu gestalten
-        tasks.map(({id, title, completed}) => (
-          <li key={id}>
-            <input type='checkbox' />
-            {title}
-            <button onClick={() => itemLoeschen(id)}>X</button>
-          </li>
-        ))
-        }
-      </ul>
-    </>
-  )
+	return (
+		<>
+			<h1>To-Do List</h1>
+			<input value={title} onChange={(e) => setTitle(e.target.value)} />
+			<button disabled={!title.trim()} onClick={itemHinzufuegen}>
+				Add
+			</button>{' '}
+			{/* Option 2 für Eingabecheck: Button wird disabled bleiben wenn das Eingabefeld leer ist*/}
+			<ul>
+				{
+					tasks.map(({ id, title, completed }) => (
+						<li key={id}>
+							<input type="checkbox" />
+							{title}
+							<button onClick={() => itemLoeschen(id)}>X</button>
+						</li>
+					))
+				}
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
